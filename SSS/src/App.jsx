23a5a603a112d407d80b6efa3ed14526ccd08cfd
Saahil <saahilmishra016@yahoo.com>
2024-11-02 23:15:36 +0530

    frontend

--- SSS/src/App.jsx
+++ SSS/src/App.jsx
@@ -6,40 +6,35 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <h1>File Shredder</h1>
+      <div className="heading">
+        <h1>
+          File Shredder <br></br> using Shameer's Secret Sharing
+        </h1>
       </div>
-      <Button
-        variant="contained"
-        component="label"
-        startIcon={<CloudUploadIcon />}
-        sx={{
-          backgroundColor: "#4CAF50", // Customize as needed
-          color: "#fff",
-          padding: "10px 20px",
-          borderRadius: "8px",
-          "&:hover": {
-            backgroundColor: "#45a049",
-          },
-        }}
-      >
-        Upload Files
-        <input
-          type="file"
-          onChange={(event) => console.log(event.target.files)}
-          multiple
-          hidden
-        />
-      </Button>
-      <div className="card">
-        <button>count</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="button-enc">
+        <Button
+          variant="contained"
+          component="label"
+          startIcon={<CloudUploadIcon />}
+          sx={{
+            backgroundColor: "#4CAF50", // Customize as needed
+            color: "#fff",
+            padding: "10px 20px",
+            borderRadius: "8px",
+            "&:hover": {
+              backgroundColor: "#45a049",
+            },
+          }}
+        >
+          Upload Files
+          <input
+            type="file"
+            onChange={(event) => console.log(event.target.files)}
+            multiple
+            hidden
+          />
+        </Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
